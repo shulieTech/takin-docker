d8b9c11be827689bc694b6c7f3b55de4ffa2eb59
VincentZhang <xiamizy@163.com>
2021-08-16 12:08:29 +0800

    Update entrypoint.sh
    modify add tro-web-url extension

--- takin-nginx/entrypoint.sh
+++ takin-nginx/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -z "$TAKIN_TRO_WEB_URL" ]; then
+     TAKIN_TRO_WEB_URL=127.0.0.1
+fi
+
 sed -e "s/var serverUrl = \'http:\/\/127.0.0.1:10008\/tro-web\/api\'/var serverUrl = 'http:\/\/$TAKIN_TRO_WEB_URL:10008\/tro-web\/api'/g" /app/dist/tro/index.html
 
 nginx
